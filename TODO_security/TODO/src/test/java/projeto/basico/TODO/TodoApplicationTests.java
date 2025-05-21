3a8d802f65c33e3eb63932e44d8a25a18b405bdb
Cazenetto <caze.netto16@gmail.com>
2025-05-21 10:15:22 -0300

    TODO com security

---
+++ TODO_security/TODO/src/test/java/projeto/basico/TODO/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.basico.TODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
